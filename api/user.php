ed58403d3214e302a189f736e36e09008ceba950
Ducanhgg123 <letrongducanh456@gmail.com>
2022-11-18 20:47:59 +0700

    asdds

--- api/user.php
+++ api/user.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: *");
 require '../database.php';
 if ($_SERVER['REQUEST_METHOD']=="POST"){
     $username=$_POST['username'];
